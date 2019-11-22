b0af73b8b8320d54a5d646354aab6a9cfda20962
Philippe <tree@gmx.ch>
2019-11-22 15:41:53 +0100

    Testclient for Grpc added

---
+++ src/TestClient/Program.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Net.Client;
+
+namespace TestClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var subsc = "all";
+
+            if (args.Length > 0)
+                subsc = args[0];
+
+            // The port number(5001) must match the port of the gRPC server.
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Magnet.Protos.Messenger.MessengerClient(channel);
+
+            AsyncServerStreamingCall<Magnet.Protos.MagnetMessage> messages =
+                client.GetMessages(new Magnet.Protos.MessagesRequest { ClientName = subsc });
+
+            Console.WriteLine("Connected, waiting for messages");
+
+            await foreach (Magnet.Protos.MagnetMessage msg in messages.ResponseStream.ReadAllAsync())
+            {
+                Console.WriteLine($"{msg.Type}-{msg.From}");
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+            Console.WriteLine("Disconecting...");
+        }
+    }
+}
