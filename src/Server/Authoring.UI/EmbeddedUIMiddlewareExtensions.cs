fa12595afeae798f171b800f1c7e333a60e5834d
bidery <yoel.bider@swisslife.ch>
2022-11-02 10:24:34 +0100

    added middleware

---
+++ src/Server/Authoring.UI/EmbeddedUIMiddlewareExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Authoring.UI;
+
+public static class EmbeddedUiMiddlewareExtensions
+{
+    public static IApplicationBuilder UseConfixUi(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<EmbeddedUiMiddleware>();
+    }
+}
+
