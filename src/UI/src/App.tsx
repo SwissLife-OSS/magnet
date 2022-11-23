e5becf692c20b2b28dc5cbf00e24d7398995a753
bidery <107406677+bidery@users.noreply.github.com>
2022-11-23 10:42:40 +0100

    commit in wrong branch

--- src/UI/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Routing from "./Routes";
-import "./App.css";
-
-const App: React.FC = () => {
-  return <Routing />;
-};
-
-export default App;
--- src/UI/src/views/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Home } from "./Home"
-export { default as NotFound } from "./NotFound"
-export { default as MessageDetail } from "./MessageDetail"
-export { default as MessageDetailError } from "./MessageDetailError"
