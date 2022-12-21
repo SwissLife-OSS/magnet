0fa86f3ed429be6edb2a793517193c9d1a89c44e
bidery <yoel.bider@swisslife.ch>
2022-12-21 16:25:11 +0100

    build script update

--- build-ui.sh
+++ build-ui.sh
@@ -2,5 +2,6 @@
 rm -rf ./src/Server/Hosting.UI/UI/
 yarn --cwd ./src/UI
 yarn --cwd ./src/UI build
+mkdir ./src/Server/Hosting.UI/UI
 cp -R ./src/UI/build/* ./src/Server/Hosting.UI/UI
 touch ./src/Server/Hosting.UI/UI/.keep
