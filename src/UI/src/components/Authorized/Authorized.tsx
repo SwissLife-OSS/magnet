3e40bca6ae5a03513bdb00da45021fd000de656a
bidery <yoel.bider@swisslife.ch>
2023-02-27 15:04:44 +0100

    implemented spinner during role check

--- src/UI/src/components/Authorized/Authorized.tsx
+++ src/UI/src/components/Authorized/Authorized.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { CircularProgress } from "@mui/material";
 import { useHasAccess } from "../../shared/components/useHasAccess";
 import { NotAuthorized as NotAuthorizedView } from "../../views";
 
 export type AuthorizedProps = React.PropsWithChildren;
 
 export const Authorized: React.FC<AuthorizedProps> = ({ children }) => {
-  return useHasAccess() ? <>{children}</> : <NotAuthorizedView />;
+  const { hasRole, isLoading } = useHasAccess();
+
+  return isLoading ? (
+    <CircularProgress />
+  ) : hasRole ? (
+    <>{children}</>
+  ) : (
+    <NotAuthorizedView />
+  );
 };
--- src/UI/src/shared/components/useHasAccess.ts
+++ src/UI/src/shared/components/useHasAccess.ts
@@ -2,7 +2,8 @@ import { getClaims } from "./getClaims";
 import { useEffect, useState } from "react";
 
 export const useHasAccess = () => {
-  const [hasRole, setHasRole] = useState<boolean>(false);
+  const [hasRole, setHasRole] = useState<boolean>();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     getClaims().then((response) => {
@@ -11,8 +12,9 @@ export const useHasAccess = () => {
       if(required){
         setHasRole(true);
       }
+      setIsLoading(false);
     })
   }, []);
 
-  return hasRole;
+  return { hasRole, isLoading };
 };
