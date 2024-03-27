7ec8726c7baab318572bedb5e5d792981c5ccc72
PascalSenn <senn.pasc@gmail.com>
2024-03-27 13:46:05 +0100

    Change build script

--- build-ui.sh
+++ build-ui.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 rm -rf ./src/Server/Hosting.UI/UI/
 yarn --cwd ./src/UI
+yarn --cwd ./src/UI relay
 yarn --cwd ./src/UI build
 mkdir ./src/Server/Hosting.UI/UI
 cp -R ./src/UI/build/* ./src/Server/Hosting.UI/UI
