3de7b87a1c1b082544cf42307c0d8e7330120d68
Mariusz Matysek <mariuszmatysek@gmail.com>
2024-05-16 07:47:40 +0200

    Handle empty strings for url and connectionString (#52)
    * Changed validation and fixed pattern matching
    
    * updated validation

--- src/Messaging.AzureServiceBus/AzureServiceBusServerBuilderExtensions.cs
+++ src/Messaging.AzureServiceBus/AzureServiceBusServerBuilderExtensions.cs
@@ -11,23 +11,12 @@ public static class AzureServiceBusServerBuilderExtensions
        this MagnetServerBuilder builder,
        IConfiguration configuration)
     {
-        AzureServiceBusOptions? azureOptions = configuration
+        AzureServiceBusOptions azureOptions = configuration
             .GetSection("Magnet:ServiceBus")
-            .Get<AzureServiceBusOptions>();
-
-        if (azureOptions == null)
-        {
-            throw new ConfigurationErrorsException(
-                "Magnet:ServiceBus section is missing in the configuration");
-        }
-
-        if (azureOptions.ConnectionString == null && azureOptions.Url == null)
-        {
-            throw new ConfigurationErrorsException(
-                "ConnectionString or Url is required for Azure Service Bus.");
-        }
-
+            .Get<AzureServiceBusOptions>()
+            .Validate();
         builder.AddAzureServiceBus(azureOptions);
+
         return builder;
     }
 
@@ -36,7 +25,7 @@ public static class AzureServiceBusServerBuilderExtensions
         this MagnetServerBuilder builder,
         AzureServiceBusOptions options)
     {
-        builder.Services.AddSingleton(options);
+        builder.Services.AddSingleton(options.Validate());
         builder.Services.AddSingleton<IMessageBus, MessageBus>();
 
         return builder;
@@ -49,8 +38,24 @@ public static class AzureServiceBusServerBuilderExtensions
     {
         var options = new AzureServiceBusOptions();
         setup.Invoke(options);
-        
+
         return builder.AddAzureServiceBus(options);
     }
 
+    private static AzureServiceBusOptions Validate(this AzureServiceBusOptions? options)
+    {
+        if (options == null)
+        {
+            throw new ConfigurationErrorsException(
+                "Magnet:ServiceBus section is missing in the configuration");
+        }
+
+        if (string.IsNullOrEmpty(options.ConnectionString) && string.IsNullOrEmpty(options.Url))
+        {
+            throw new ConfigurationErrorsException(
+                "ConnectionString or Url is required for Azure Service Bus.");
+        }
+
+        return options;
+    }
 }
