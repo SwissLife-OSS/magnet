a4b2841d4e5d8061639a59d9da346a836479805e
Gabriel Lucaci <gabriel.lucaci.dev@gmail.com>
2025-03-31 13:49:34 +0200

    Add root dir for the build-ui script (#54)

--- build-ui.sh
+++ build-ui.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-rm -rf ./src/Server/Hosting.UI/UI/
-yarn --cwd ./src/UI
-yarn --cwd ./src/UI build-prod
-mkdir ./src/Server/Hosting.UI/UI
-cp -R ./src/UI/build/* ./src/Server/Hosting.UI/UI
-touch ./src/Server/Hosting.UI/UI/.keep
+
+# Use provided ROOT_DIR or default to current directory
+ROOT_DIR="${ROOT_DIR:-./}"
+
+rm -rf $ROOT_DIR/src/Server/Hosting.UI/UI/
+yarn --cwd $ROOT_DIR/src/UI
+yarn --cwd $ROOT_DIR/src/UI build-prod
+mkdir $ROOT_DIR/src/Server/Hosting.UI/UI
+cp -R $ROOT_DIR/src/UI/build/* $ROOT_DIR/src/Server/Hosting.UI/UI
+touch $ROOT_DIR/src/Server/Hosting.UI/UI/.keep
